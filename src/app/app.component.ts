ffd3e23023a833bc2225c034ea000af68f644e4a
Shreevardhan <svdamani@gmail.com>
2017-07-14 20:07:19 +0530

    Added active CSS class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DummyService } from "app/dummy.service";
+import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,12 +9,20 @@ import { DummyService } from "app/dummy.service";
 })
 export class AppComponent {
   all = {}
+  selectedId: string
 
   constructor(
-    private dummyService: DummyService
+    private dummyService: DummyService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.all = this.dummyService.getAll()
+
+    this.router.events.subscribe(val=> {
+      if (val instanceof NavigationEnd) {
+        this.selectedId = this.router.parseUrl(this.router.url).root.children.primary.segments[1].path
+      }
+    })
   }
 }
